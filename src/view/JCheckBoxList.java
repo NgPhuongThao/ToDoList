783d4392ffd44cd1c24731316260cf9b8e138851
NgPhuongThao <nguyen.phuongthaof@gmail.com>
2023-07-04 21:08:40 +0200

    Cross out ticked task Corrected

--- src/view/JCheckBoxList.java
+++ src/view/JCheckBoxList.java
@@ -1,4 +1,5 @@
 package view;
+
 import java.awt.Component;
 import java.awt.Font;
 
@@ -11,68 +12,62 @@ import java.util.Map;
 
 @SuppressWarnings("serial")
 public class JCheckBoxList extends JList<JCheckBox> {
-  protected static Border noFocusBorder = new EmptyBorder(1, 1, 1, 1);
-  private boolean ticked = false;
+	protected static Border noFocusBorder = new EmptyBorder(1, 1, 1, 1);
 
-  public JCheckBoxList() {
-    setCellRenderer(new CellRenderer());
-    addMouseListener(new MouseAdapter() {
-      public void mousePressed(MouseEvent e) {
-        int index = locationToIndex(e.getPoint());
-        if (index != -1 && e.getX() < 15) {
-          JCheckBox checkbox = (JCheckBox) getModel().getElementAt(index);
-          checkbox.setSelected(!checkbox.isSelected());
-          
-          ((DefaultListModel<JCheckBox>) getModel()).removeElementAt(index);
-          if (checkbox.isSelected()) { // If task done
-        	  // Cross out the task
-        	  @SuppressWarnings("unchecked")
-        	  Map<TextAttribute, Boolean> attributes = (Map<TextAttribute, Boolean>) getFont().getAttributes();
-        	  attributes.put(TextAttribute.STRIKETHROUGH, TextAttribute.STRIKETHROUGH_ON);
-        	  checkbox.setFont(new Font(attributes));
-        	  
-        	  ((DefaultListModel<JCheckBox>) getModel()).addElement(checkbox); // Put Task last
-          }
-          else {
-        	  // Remove crossing out the task
-        	  @SuppressWarnings("unchecked")
-        	  Map<TextAttribute, Boolean> attributes = (Map<TextAttribute, Boolean>) getFont().getAttributes();
-        	  attributes.remove(TextAttribute.STRIKETHROUGH);
-        	  checkbox.setFont(new Font(attributes));
-        	  
-        	  ((DefaultListModel<JCheckBox>) getModel()).add(0,checkbox); // Put Task first
-          }
-          ticked = true;
-          repaint();
-        }
-      }
-    });
-    setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-  }
+	public JCheckBoxList() {
+		
+		setCellRenderer(new CellRenderer());
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				int index = locationToIndex(e.getPoint());
+				if (index != -1 && e.getX() < 15) {
+					JCheckBox checkbox = (JCheckBox) getModel().getElementAt(index);
+					checkbox.setSelected(!checkbox.isSelected());
 
-  public JCheckBoxList(ListModel<JCheckBox> model){
-    this();
-    setModel(model);
-  }
+					((DefaultListModel<JCheckBox>) getModel()).removeElementAt(index);
 
-  protected class CellRenderer implements ListCellRenderer<JCheckBox> {
-    public Component getListCellRendererComponent(
-        JList<? extends JCheckBox> list, JCheckBox value, int index,
-        boolean isSelected, boolean cellHasFocus) {
-      JCheckBox checkbox = value;
+					if (checkbox.isSelected()) // If task done
+						((DefaultListModel<JCheckBox>) getModel()).addElement(checkbox); // Put Task last
+					 else 
+						((DefaultListModel<JCheckBox>) getModel()).add(0, checkbox); // Put Task first
 
-      //Drawing checkbox, change the appearance here
-      checkbox.setBackground(isSelected ? Utils.ACCENT_COLOR
-          : getBackground());
-      checkbox.setForeground(isSelected ? Utils.BACKGROUND_COLOR
-          : getForeground());
-      checkbox.setEnabled(isEnabled());
-      checkbox.setFont(ticked && isSelected ? checkbox.getFont() : getFont());
-      checkbox.setFocusPainted(false);
-      checkbox.setBorderPainted(true);
-      checkbox.setBorder(isSelected ? UIManager
-          .getBorder("List.focusCellHighlightBorder") : noFocusBorder);
-      return checkbox;
-    }
-  }
+					repaint();
+				}
+			}
+		});
+		setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+	}
+
+	public JCheckBoxList(ListModel<JCheckBox> model) {
+		this();
+		setModel(model);
+	}
+
+	protected class CellRenderer implements ListCellRenderer<JCheckBox> {
+		public Component getListCellRendererComponent(JList<? extends JCheckBox> list, JCheckBox value, int index,
+				boolean isSelected, boolean cellHasFocus) {
+			JCheckBox checkbox = value;
+
+			// Drawing checkbox, change the appearance here
+			checkbox.setBackground(isSelected ? Utils.ACCENT_COLOR : getBackground());
+			checkbox.setForeground(isSelected ? Utils.BACKGROUND_COLOR : getForeground());
+			checkbox.setEnabled(isEnabled());
+			
+			if (!checkbox.isSelected()) {
+				checkbox.setFont(getFont());
+			} else {
+				@SuppressWarnings("unchecked")
+				Map<TextAttribute, Boolean> attributes = (Map<TextAttribute, Boolean>) getFont()
+					.getAttributes();
+				attributes.put(TextAttribute.STRIKETHROUGH, TextAttribute.STRIKETHROUGH_ON);
+				checkbox.setFont(new Font(attributes));
+				
+			}
+			
+			checkbox.setFocusPainted(false);
+			checkbox.setBorderPainted(true);
+			checkbox.setBorder(isSelected ? UIManager.getBorder("List.focusCellHighlightBorder") : noFocusBorder);
+			return checkbox;
+		}
+	}
 }
